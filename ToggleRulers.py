97b055f58b4de19d698c6df1d02ff43e9411c2f0
Zachary Halle <zhalle@cmu.edu>
2015-04-28 14:03:10 -0400

    Modifed rulers command

--- ToggleRulers.py
+++ ToggleRulers.py
@@ -3,7 +3,7 @@ import sublime, sublime_plugin
 class ToggleRulersCommand(sublime_plugin.TextCommand):
 	def run(self, edit, **kwargs):
 		if self.view.settings().get("rulers") == []:
-			rulers = kwargs["values"]
+			rulers = kwargs["rulers"]
 		else: 
 			rulers = []
 		self.view.settings().set("rulers", rulers)
